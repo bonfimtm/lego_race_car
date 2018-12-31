ea50659f55ee0d1ebacd566541c52bba07fdac27
Thiago Bonfim <bonfimtm@gmail.com>
2018-12-31 18:10:17 +0100

    Define macros for microcontroller pins

--- lego_race_car.ino
+++ lego_race_car.ino
@@ -12,9 +12,19 @@
 
 #define SERIAL_BAUD_RATE 9600
 
-RemoteControl remoteControl(A0);
-Powertrain powertrain(9, 8, 7, &remoteControl);
-Steering steering(6, 5, 4, &remoteControl);
+#define REMOTE_CONTROL_RECEIVER_PIN A0
+
+#define POWERTRAIN_PWM_PIN 10
+#define POWERTRAIN_DIR_PIN_A 9
+#define POWERTRAIN_DIR_PIN_B 8
+
+#define STEERING_PWM_PIN 6
+#define STEERING_DIR_PIN_A 5
+#define STEERING_DIR_PIN_B 4
+
+RemoteControl remoteControl(REMOTE_CONTROL_RECEIVER_PIN);
+Powertrain powertrain(POWERTRAIN_PWM_PIN, POWERTRAIN_DIR_PIN_A, POWERTRAIN_DIR_PIN_B, &remoteControl);
+Steering steering(STEERING_PWM_PIN, STEERING_DIR_PIN_A, STEERING_DIR_PIN_B, &remoteControl);
 
 // The setup function runs once when you press reset or power the board
 void setup()
